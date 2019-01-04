87ad999b1c60472233b9f9daa8de14cf5511b581
Deepak Sharma <TechExhibeo@gmail.com>
2019-01-04 13:33:01 +0530

    Backup Current cronjobs

--- cron.sh
+++ cron.sh
@@ -6,5 +6,10 @@ CRON_TIME="$1"
 CRON_CMD="bash /root/gdrive-backup-cpanel/backup.sh >> /root/gdrive-backup-cpanel/backup.log 2>&1"
 CRON_JOB="$CRON_TIME $CRON_CMD"
 
+# Backup Current cronjobs to file
+
+crontab -l >> /root/cron.jobs.list
+
 # Add cron rule in crontab without any duplicate
+
 ( crontab -l | grep -v -F "$CRON_CMD" ; echo "$CRON_JOB" ) | crontab -
