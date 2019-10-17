9d70c26af55aafbbd78c71bf3e84b1560e88e381
Deepak Sharma <TechExhibeo@gmail.com>
2019-10-17 18:09:52 +0530

    run single account backup creation of single account backup followed by pushing to google drive

--- README.md
+++ README.md
@@ -3,13 +3,13 @@ Automate Uploading of cPanel Created Backups to Google Drive using rclone
 
 1) Clone this repo in /root directory of the server with directory name gdrive-backup-cpanel using following Command
  
- > git clone https://github.com/xaksh/gdrive-backup-cpanel -b cpanel+gdrive gdrive-backup-cpanel
+ > git clone https://github.com/xaksh/gdrive-backup-cpanel -b cpanel+gdrive+jbc gdrive-backup-cpanel
  
  or you can use wget to download Zip Archive and Extract.
  
- > wget 'https://github.com/xaksh/gdrive-backup-cpanel/archive/cpanel+gdrive.zip'
+ > wget 'https://github.com/xaksh/gdrive-backup-cpanel/archive/cpanel+gdrive+jbc.zip'
  
- > unzip -j cpanel+gdrive.zip -d gdrive-backup-cpanel
+ > unzip -j cpanel+gdrive+jbc.zip -d gdrive-backup-cpanel
 
 2) Run setup.sh with Server Hostname, Host Node, Cron Job Time as arguments
 
--- backup.sh
+++ backup.sh
@@ -6,16 +6,25 @@ BACKUP_DIR=/backup
 SERVER_HOSTNAME=
 NODE=
 
-echo "~~~~~~~~~~~~~~ Starting Uploading BACKUP ~~~~~~~~~~~~~~"
-echo $DATE
+# Create fresh backup directory
+mkdir -p "$BACKUP_DIR/$DATE"
 
-# Upload backup files to respected Directory in Google Drive
-time rclone copy $BACKUP_DIR/$DATE gdrive:basezap"$NODE"nodebackups/$SERVER_HOSTNAME/$DATE
+echo "~~~~~~~~~~~~~~ Starting BACKUP Creation and Upload to Google Drive ~~~~~~~~~~~~~~"
+echo $DATE
+start=$SECONDS
+ls -1 /var/cpanel/users | while read user; do
+/scripts/pkgacct $user $BACKUP_DIR/$DATE --backup --skiplogs --skipbwdata --nocompress > /dev/null
 wait
+rclone copy $BACKUP_DIR/$DATE gdrive:basezap"$NODE"nodebackups/$SERVER_HOSTNAME/$DATE
+wait
+rm -f $BACKUP_DIR/$DATE/*
+wait
+done
+echo "~~~~~~~~~~~~~~ Backup Creation and Upload to Google Drive Finished ~~~~~~~~~~~~~~"
+duration=$(( SECONDS - start ))
+echo "Total Time Taken $duration Seconds"
 
 # Remove backup directory
-echo "~~~~~~~~~~~~~~ Removing BACKUP Files ~~~~~~~~~~~~~~"
-time rm -rf -v $BACKUP_DIR/$DATE
-wait
+rm -rf $BACKUP_DIR/$DATE
 
 exit
